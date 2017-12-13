85030777ef15cf0129a2641bc3a01f390274e7ef
Drew Powers <drew@pow.rs>
2017-12-13 11:32:45 -0500

    Add webpack DefinePlugin to production

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -13,6 +13,9 @@ module.exports = merge(common, {
   },
   devtool: "inline-source-map",
   plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production")
+    }),
     new UglifyJSPlugin({
       sourceMap: true
     }),
